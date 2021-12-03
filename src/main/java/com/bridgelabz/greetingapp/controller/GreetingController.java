49ea849dbf7142ef282ef3e1e3f8ce312bbeaf29
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-12-03 22:24:29 +0530

    Added: Performed various HTTP methods using JSON

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,12 +1,27 @@
 package com.bridgelabz.greetingapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.greetingapp.dto.UserDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
     @GetMapping(value = "/greet")
     public String getMessage() {
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping(value = "/message")
+    public String getGreeting(
+            @RequestParam String firstName,
+            @RequestParam String lastName
+    ) {
+        return "Hello" + firstName + " " + lastName;
+    }
+
+    @PostMapping(value = "/getGreet")
+    public UserDTO userDetails(
+            @RequestBody UserDTO userDTO
+    ) {
+        return userDTO;
     }
 }
---
+++ src/main/java/com/bridgelabz/greetingapp/dto/UserDTO.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.greetingapp.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+
+}
